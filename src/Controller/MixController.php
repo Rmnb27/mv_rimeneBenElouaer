d42ad475a60eea74d184dff60e34e7edbb1d9a90
Rmnb27 <rimenebenelouaer@gmail.com>
2024-05-06 15:04:36 +0200

    Param Converter & 404's

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\VinylMix;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\VinylMixRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Doctrine\ORM\EntityManagerInterface;
-use App\Repository\VinylMixRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MixController extends AbstractController
 {
@@ -36,12 +37,24 @@ class MixController extends AbstractController
         )); 
     }
     #[Route('/mix/{id}', name: 'app_mix_show')]
-    public function show($id, VinylMixRepository $mixRepository): Response
+    public function show(VinylMix $mix): Response
     {
-        $mix = $mixRepository->find($id);
-        //dd($mix);
+       
         return $this->render('mix/show.html.twig', [
             'mix' => $mix,
         ]);
+    }
+    #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
+    public function vote(VinylMix $mix, Request $request , EntityManagerInterface $entityManager): Response
+    {
+        $direction = $request->request->get('direction', 'up');
+        if ($direction === 'up') {
+            $mix->setVotes($mix->getVotes() + 1);
+        } else {
+            $mix->setVotes($mix->getVotes() - 1);
+        }
+        
+        $entityManager->flush();
+        dd($mix);
     }
 }
