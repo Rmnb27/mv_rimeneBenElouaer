dad888f3c02be376e3e86d037822c7307e2003b8
Rmnb27 <rimenebenelouaer@gmail.com>
2024-05-06 14:15:48 +0200

    Querying for a Single Entity for a "Show" Page".

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\VinylMixRepository;
 
 class MixController extends AbstractController
 {
@@ -17,6 +18,9 @@ class MixController extends AbstractController
     {
         $mix = new VinylMix();
         $mix->setTitle('Do you Remember... Phil Collins?!');
+        $genres = ['pop', 'rock'];
+        $mix->setGenre($genres[array_rand($genres)]);
+        
         $mix->setDescription('A pure mix of drummers turned singers!');
         $mix->setGenre('pop');
         $mix->setTrackCount(rand(5, 20));
@@ -30,5 +34,14 @@ class MixController extends AbstractController
             $mix->getTrackCount() 
 
         )); 
+    }
+    #[Route('/mix/{id}', name: 'app_mix_show')]
+    public function show($id, VinylMixRepository $mixRepository): Response
+    {
+        $mix = $mixRepository->find($id);
+        //dd($mix);
+        return $this->render('mix/show.html.twig', [
+            'mix' => $mix,
+        ]);
     }
 }
